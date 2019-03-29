c17da420728028e8018e68534fab44fafdfa781b
System Administrator <root@C02Y707QJGH7.verizon.net>
2019-03-29 13:00:52 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            I have successfully changed the compose-file to mount local volume to the container! This is my first React NodeJs APP.....
           </p>
           <a
             className="App-link"
